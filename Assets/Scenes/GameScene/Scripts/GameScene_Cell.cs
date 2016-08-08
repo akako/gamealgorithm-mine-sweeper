d0e4a24173e5bc5503019a31efb9051397dfb540
賀好昭仁 <kako@qnote.co.jp>
2016-08-08 17:45:46 +0900

    [add]ファーストコミット

---
+++ Assets/Scenes/GameScene/Scripts/GameScene_Cell.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+/// <summary>
+/// セル
+/// </summary>
+public class GameScene_Cell : Button
+{
+    public int X { get { return x; } }
+
+    public int Y { get { return y; } }
+
+    public bool IsOpened { get { return !overlay.activeSelf; } }
+
+    public bool IsChecked { get { return checkMark.activeSelf; } }
+
+    public bool HasBom { get { return bom.activeSelf; } }
+
+    [SerializeField]
+    Text number;
+    [SerializeField]
+    GameObject bom;
+    [SerializeField]
+    GameObject overlay;
+    [SerializeField]
+    GameObject checkMark;
+
+    GameScene_Controller controller;
+    int x;
+    int y;
+
+    protected override void Start()
+    {
+        base.Start();
+        onClick.AddListener(OnClick);
+    }
+
+    public void Initialize(GameScene_Controller controller, int x, int y)
+    {
+        this.controller = controller;
+        this.x = x;
+        this.y = y;
+
+        number.gameObject.SetActive(true);
+        bom.SetActive(false);
+        overlay.SetActive(true);
+        checkMark.SetActive(false);
+    }
+
+    public void SetBom(bool hasBom)
+    {
+        bom.SetActive(hasBom);
+        number.gameObject.SetActive(!hasBom);
+    }
+
+    public bool Open()
+    {
+        if (IsOpened || IsChecked)
+        {
+            // オープン済みorチェックされたマスなら何もしない
+            return true;
+        }
+
+        Reveal();
+        if (HasBom)
+        {
+            return false;
+        }
+
+        var aroundCells = controller.GetAroundCells(this);
+        var bomCountOnAroundCell = aroundCells.Count(x => x.HasBom);
+        if (bomCountOnAroundCell == 0)
+        {
+            number.text = "";
+            foreach (var cell in aroundCells.Where(c => (Mathf.Abs(c.X - x) + Mathf.Abs(c.Y - y)) == 1))
+            {
+                if (!cell.Open())
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            number.text = string.Format("{0}", bomCountOnAroundCell);
+        }
+
+        return true;
+    }
+
+    public void Reveal()
+    {
+        overlay.SetActive(false);
+    }
+
+    void OnClick()
+    {
+        if (IsOpened)
+        {
+            // 既にオープンされたマスなら何もしない
+            return;
+        }
+        switch (controller.Mode)
+        {
+            case GameScene_Controller.Modes.Open:
+                if (!Open())
+                {
+                    controller.GameOver();
+                }
+                break;
+            case GameScene_Controller.Modes.Check:
+                checkMark.SetActive(!checkMark.activeSelf);
+                break;
+        }
+    }
+}
---
+++ Assets/Scenes/GameScene/Scripts/GameScene_Controller.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// ゲームシーンコントローラ
+/// </summary>
+public class GameScene_Controller : MonoBehaviour
+{
+    public enum Modes
+    {
+        // マスを開くモード
+        Open,
+        // マスをチェック状態にするモード
+        Check
+    }
+
+    public Modes Mode { get { return mode; } }
+
+    bool IsGameOver { get { return gameOverText.activeSelf; } }
+
+    [SerializeField]
+    Size size;
+    [SerializeField]
+    Button setOpenButton;
+    [SerializeField]
+    Button setCheckButton;
+    [SerializeField]
+    GridLayoutGroup cellContainer;
+    [SerializeField]
+    GameObject gameOverText;
+    [SerializeField]
+    GameScene_Cell cellPrefab;
+
+    Modes mode;
+    List<GameScene_Cell> cells = new List<GameScene_Cell>();
+
+    void Start()
+    {
+        setOpenButton.onClick.AddListener(OnSetOpenButtonClick);
+        setCheckButton.onClick.AddListener(OnSetCheckButtonClick);
+        OnSetOpenButtonClick();
+
+        Initialize();
+    }
+
+    void Update()
+    {
+        if (IsGameOver && Input.GetMouseButtonDown(0))
+        {
+            Initialize();
+        }
+    }
+
+    public void Initialize()
+    {
+        gameOverText.SetActive(false);
+        cellPrefab.gameObject.SetActive(true);
+
+        foreach (var cell in cells)
+        {
+            Destroy(cell.gameObject);
+        }
+        cells.Clear();
+
+        var cellContainerRect = cellContainer.GetComponent<RectTransform>();
+        cellContainer.cellSize = new Vector2(cellContainerRect.sizeDelta.x / size.x, cellContainerRect.sizeDelta.y / size.y);
+        for (var x = 0; x < size.x; x++)
+        {
+            for (var y = 0; y < size.y; y++)
+            {
+                var cell = Instantiate(cellPrefab);
+                cell.Initialize(this, x, y);
+                cell.transform.SetParent(cellContainer.transform);
+                cells.Add(cell);
+            }
+        }
+        for (var i = 0; i < size.x * size.y * 0.1; i++)
+        {
+            var noBomSells = cells.Where(x => !x.HasBom);
+            noBomSells.ElementAt(UnityEngine.Random.Range(0, noBomSells.Count())).SetBom(true);
+        }
+        cellPrefab.gameObject.SetActive(false);
+    }
+
+    public List<GameScene_Cell> GetAroundCells(GameScene_Cell targetCell)
+    {
+        var aroundCells = new List<GameScene_Cell>();
+        Action<int, int> addCellIfExists = (x, y) =>
+        {
+            var cell = cells.FirstOrDefault(c => c.X == x && c.Y == y);
+            if (null != cell)
+            {
+                aroundCells.Add(cell);
+            }
+        };
+        addCellIfExists(targetCell.X - 1, targetCell.Y - 1);
+        addCellIfExists(targetCell.X - 1, targetCell.Y);
+        addCellIfExists(targetCell.X - 1, targetCell.Y + 1);
+        addCellIfExists(targetCell.X, targetCell.Y - 1);
+        addCellIfExists(targetCell.X, targetCell.Y + 1);
+        addCellIfExists(targetCell.X + 1, targetCell.Y - 1);
+        addCellIfExists(targetCell.X + 1, targetCell.Y);
+        addCellIfExists(targetCell.X + 1, targetCell.Y + 1);
+        return aroundCells;
+    }
+
+    /// <summary>
+    /// ゲームオーバー処理
+    /// </summary>
+    public void GameOver()
+    {
+        gameOverText.SetActive(true);
+        foreach (var cell in cells.Where(x => x.HasBom))
+        {
+            cell.Reveal();
+        }
+    }
+
+    void OnSetOpenButtonClick()
+    {
+        mode = Modes.Open;
+        setOpenButton.image.color = Color.red;
+        setCheckButton.image.color = Color.white;
+    }
+
+    void OnSetCheckButtonClick()
+    {
+        mode = Modes.Check;
+        setOpenButton.image.color = Color.white;
+        setCheckButton.image.color = Color.red;
+    }
+
+    [Serializable]
+    class Size
+    {
+        public int x = 10;
+        public int y = 10;
+    }
+}
